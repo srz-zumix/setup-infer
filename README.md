974cee04bf330acbc6fe24fdcfb9177d92524e12
srz_zumix <zumix.cpp@gmail.com>
2021-11-06 12:57:56 +0900

    first commit (#1)

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # setup-infer
+
+[![GitHub Actions - setup check](https://github.com/srz-zumix/setup-infer/actions/workflows/main.yml/badge.svg)](https://github.com/srz-zumix/setup-infer/actions/workflows/main.yml)
+[![GitHub Actions - reviewdog](https://github.com/srz-zumix/setup-infer/actions/workflows/reviewdog.yml/badge.svg)](https://github.com/srz-zumix/setup-infer/actions/workflows/reviewdog.yml)
+
+This action installs :blue_book: [infer][].
+
+## Input
+
+```yaml
+inputs:
+  infer_version:
+    description: 'infer version. [latest,vX.Y.Z]'
+    default: 'latest'
+```
+
+## Usage
+
+### Latest
+
+```yaml
+steps:
+  - uses: srzzumix/setup-infer@master
+  - run: infer --version
+```
+
+### Specify infer version
+
+NOTE: macOS always install latest
+
+```yaml
+steps:
+  - uses: srzzumix/setup-infer@master
+    with:
+      infer_version: v1.0.0
+  - run: infer --version
+```
+
+[infer]:https://github.com/facebook/infer
---
+++ install.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+VERSION="${INFER_VERSION:-latest}"
+
+source "${GITHUB_ACTION_PATH}/utils.sh"
+
+TEMP="${INFER_TEMPDIR}"
+if [ -z "${TEMP}" ]; then
+  if [ -n "${RUNNER_TEMP}" ]; then
+    TEMP="${RUNNER_TEMP}"
+  else
+    TEMP="$(mktemp -d)"
+  fi
+fi
+
+if [ "${VERSION}" == 'latest' ] ; then
+  VERSION=$(curl -s https://api.github.com/repos/facebook/infer/releases/latest | grep "tag_name" | grep -o "v[0-9.]*")
+fi
+
+install_osx() {
+    brew install infer
+}
+
+install_linux() {
+    cd "${TEMP}" || exit
+    curl -sL "https://github.com/facebook/infer/releases/download/${VERSION}/infer-linux64-${VERSION}.tar.xz" | tar xvJ
+    echo "${TEMP}/infer-linux64-${VERSION}/bin" >>"${GITHUB_PATH}"
+}
+
+install_windows() {
+    echo "windows is not supported"
+    exit 1
+}
+
+install() {
+    if is_osx; then
+        install_osx
+    fi
+    if is_linux; then
+        install_linux
+    fi
+    if is_windows; then
+        install_windows
+    fi
+}
+echo '::group::📖 Installing infer ...'
+install
+echo '::endgroup::'
