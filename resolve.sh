44332fdc4906a3cb4e844f0183c1b05d08ab1d5c
srz_zumix <zumix.cpp@gmail.com>
2024-03-28 23:20:48 +0900

    up

--- resolve.sh
+++ resolve.sh
@@ -3,12 +3,19 @@ set -euox pipefail
 
 VERSION="${INPUTS_INFER_VERSION:-latest}"
 
+resolve_version() {
+  for i in {1..5}; do
+    VERSION=$(curl --retry 3 -s https://api.github.com/repos/facebook/infer/releases/latest | grep "tag_name" | grep -o "v[0-9.]*" || true)
+    if [ -n "${VERSION}" ]; then
+      return
+    fi
+  done
+  echo "::error:: Failed to get latest version from GitHub API"
+  exit 1
+}
+
 if [ "${VERSION}" == 'latest' ] ; then
-  VERSION=$(curl --retry 3 -s https://api.github.com/repos/facebook/infer/releases/latest | grep "tag_name" | grep -o "v[0-9.]*" || true)
-  if [ -z "${VERSION}" ]; then
-    echo "::error:: Failed to get latest version from GitHub API"
-    exit 1
-  fi
+  resolve_version
 fi
 
 echo "version=${VERSION}" >> "${GITHUB_OUTPUT}"
